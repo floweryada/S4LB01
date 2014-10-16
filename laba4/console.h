9ec49a5fd7b8eaccdb84eef9f6a97965e91fcd25
floweryada <floweryada@gmail.com>
2014-10-16 23:59:49 +0300

    z

---
+++ laba4/console.h
@@ -0,0 +1,17 @@
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+class Console:
+
+    public Tree
+    {
+    private:
+        virtual void ShowTree();
+
+    public:
+        void ShowAll();
+    };
+
+#endif // CONSOLE_H
+
+
